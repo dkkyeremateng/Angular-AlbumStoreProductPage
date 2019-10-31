756d959ead8e38ce3f00b5b4e8d6dbcf96e80056
Daniel Kofi Kyeremateng <kofwayne305@gmail.com>
2019-10-31 11:18:15 +0000

    Adding Product List Component

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Product } from '../product';
+import { ProductService } from '../product.service';
+
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent implements OnInit {
+  constructor(private _productService: ProductService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
